bddb7b39b5ae5f9a66187da0e358f9c9fe742831
Tim hvaal <timothy.hvaal@bluefruit.co.uk>
2023-06-05 10:10:16 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace GPT_Assay_Chooser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.blah = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // blah
+            // 
+            this.blah.Location = new System.Drawing.Point(305, 96);
+            this.blah.Name = "blah";
+            this.blah.Size = new System.Drawing.Size(75, 23);
+            this.blah.TabIndex = 0;
+            this.blah.Text = "blahbutton";
+            this.blah.UseVisualStyleBackColor = true;
+            this.blah.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 523);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.blah);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button blah;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ExampleGUI
+{
+    // Main entry point
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            // When button is clicked, print "Hello World"
+            Console.WriteLine("Hello World!");
+        }
+
+        private void InitializeComponent()
+        {
+            // Create a button and set the Text property to "Click Here" 
+            // and the Location and Size properties to desired values
+            this.button1 = new System.Windows.Forms.Button();
+            this.button1.Text = "Submit User Text";
+            this.button1.Location = new System.Drawing.Point(113, 113);
+            this.button1.Size = new System.Drawing.Size(75, 75);
+
+            // Connect the Click event of the button to the Event Handler
+            this.button1.Click += new System.EventHandler(this.Button_Click);
+
+            // Set the size of the form
+            this.ClientSize = new System.Drawing.Size(284, 600);
+
+            // Add the button to the form
+            this.Controls.Add(this.button1);
+
+            // Set the name of the form
+            this.Name = "MainForm";
+        }
+
+        private System.Windows.Forms.Button button1;
+    }
+}
